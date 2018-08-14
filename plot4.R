441de09e464b6dfcc0454f2d622c6a40df497a9f
byanuaria <39754202+byanuaria@users.noreply.github.com>
2018-08-14 12:58:05 -0700

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,35 @@
+# reading in data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# load packages
+library(tidyverse)
+
+# convert SCC from factor class to character 
+SCC$SCC <- as.character(SCC$SCC)
+NEI$year <- as.character(NEI$year)
+NEI$type <- as.factor(NEI$type)
+
+# use regex to index observations related to coal combustion
+ind <- grep(pattern = "Coal", x = SCC$EI.Sector)
+new <- SCC[ind,]
+
+# get vector of related Source Classification Code (SCC)
+mySCCs <- new$SCC
+
+# filter data from NEI table
+myDF <- NEI %>%
+  select(SCC, type, year, Emissions) %>%
+  filter(SCC %in% mySCCs)
+
+# create PNG file
+png('plot4.png')
+
+# plot
+ggplot(myDF, aes(year, Emissions)) +
+  geom_jitter(aes(color = type), alpha = .6) +
+  labs(title = "Emissions from Coal Combustion Sources",
+       x = "Year", y = "Emissions") +
+  theme_minimal()
+
+dev.off()
