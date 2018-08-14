79ba05e7f25e2e0226d4ad824878a504a196776b
byanuaria <39754202+byanuaria@users.noreply.github.com>
2018-08-14 14:44:42 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,6 @@
+# Of the four types of sources indicated by the type variable, which of these four
+# sources have seen decreases in emissions from 1999-2008 for Baltimore City?
+
 # read in data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
