989ad59969b97f41f33a98650eef8d7fc024f335
byanuaria <39754202+byanuaria@users.noreply.github.com>
2018-08-13 21:54:38 -0700

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,33 @@
+# loading data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# loading packages 
+library(tidyverse)
+require(scales)
+
+# plot showing the total PM2.5 emission from all sources
+# (type) for each of the years 1999, 2002, 2005 and 2008
+
+#convert type to factor class
+NEI$type <- as.factor(NEI$type)
+
+# selecting data I want
+my_df <- NEI %>%
+  select(Emissions, type, year) %>%
+  group_by(year, type) %>%
+  summarize(total_E = sum(Emissions))
+
+# create PNG file
+png('plot1.png')
+
+# create line plot
+l <- ggplot(my_df, aes(year, total_E)) +
+  geom_point(aes(color = type), alpha = 0.7, size = 2) + # adds point
+  geom_smooth(aes(color = type)) + # adds line
+  scale_y_continuous(labels = comma) + # gets rid of default scientific notation
+  labs(title = "Emissions Throughout The Years", y = "Total Emissions", x = "Year")
+
+print(l)
+
+dev.off()
