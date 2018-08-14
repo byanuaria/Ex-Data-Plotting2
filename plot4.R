22f748510aa36a6f0cf8f623a68fb240a474f7e3
byanuaria <39754202+byanuaria@users.noreply.github.com>
2018-08-14 14:45:45 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,3 +1,6 @@
+# Across the United States, how have emissions from coal combustion-related 
+# sources changed from 1999-2008?
+
 # read in data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
