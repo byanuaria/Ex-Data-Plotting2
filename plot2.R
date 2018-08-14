80a2863ea0be7965514d02cf728e16c240e2a53d
byanuaria <39754202+byanuaria@users.noreply.github.com>
2018-08-14 14:38:35 -0700

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,21 +1,21 @@
-# reading in data
+# read in data
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
 # load packages
 library(tidyverse)
 
-# create PNG file
-png('plot2.png')
-
-# filtering for Baltimore City, Maryland data
+# filter for Baltimore City, Maryland data
 mdEmissions <- NEI %>%
   select(fips, Emissions, year) %>%       
   filter(fips == "24510") %>%
   group_by(year, fips) %>%
   summarize(totalEmi = sum(Emissions))
 
-# plotting
+# create PNG file
+png('plot2.png')
+
+# plot
 ggplot(mdEmissions, aes(x = year, y = totalEmi)) +    
   geom_point(shape = 1, size = 3) +                               # adding points
   geom_smooth(mapping = aes(linetype = "MD"), color = "purple") + # adding line
