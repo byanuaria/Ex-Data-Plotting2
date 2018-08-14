7aa5cf053df208eed26f36eabc1609ac77e70711
byanuaria <39754202+byanuaria@users.noreply.github.com>
2018-08-14 11:08:07 -0700

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,30 @@
+# reading in data
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# load packages
+library(tidyverse)
+
+# convert as factor
+NEI$type <- as.factor(NEI$type)
+
+# filtering data
+MD_emissions2 <- NEI %>%
+  select(fips, Emissions, year, type) %>%
+  filter(fips == "24510") %>%
+  group_by(year, type) %>%
+  summarize(total = sum(Emissions))
+
+# create PNG file
+png('plot3.png')
+
+# plot
+ggplot(MD_emissions2, aes(year, total, color = type)) +
+  geom_point(size = 2, alpha = 0.5) +
+  geom_smooth() +
+  labs(title = "Emissions by Source: 1999 - 2008", 
+       x = "Year", y = "Emissions (by tons)") +
+  theme_bw()
+
+dev.off()
+
